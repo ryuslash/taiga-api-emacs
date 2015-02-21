5c167b9153c448b4773cddc4953445b289d6fdea
Tom Willemse <tom@ryuslash.org>
2015-02-21 22:20:31 +0100

    Revert "Report on coverage in /lisp"
    This reverts commit 1c0e5df78b6668433e7ef428077ffc960d98f729.

--- tests/taiga-api-tests.el
+++ tests/taiga-api-tests.el
@@ -27,7 +27,7 @@
 (require 'ert)
 (require 'undercover)
 
-(undercover "lisp/*.el" (:exclude "*-tests.el"))
+(undercover "*.el" "lisp/*.el" "../lisp/*.el" (:exclude "*-tests.el"))
 
 (require 'taiga-api)
 
